6a6732fc34be4e8262f1d9b4cd068217454cdc2b
Mateusp_rezendee <mateusp-rezende>
2025-07-02 22:35:01 -0300

    inserção classe Cliente e main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        Cliente cliente = new Cliente();
+
+        System.out.println("Digite o numero da conta: ");
+        cliente.setNumero(scan.nextInt());
+        scan.nextLine();
+
+        System.out.println("Digite o nome do cliente: ");
+        cliente.setNomeCliente(scan.nextLine());
+
+        System.out.println("Digite a agencia: ");
+        cliente.setAgencia(scan.nextLine());
+
+        System.out.println("Digite o saldo: ");
+        cliente.setSaldo(scan.nextDouble());
+
+
+        System.out.println("\nOlá " + cliente.getNomeCliente() +
+                ", obrigado por criar uma conta em nosso banco, sua agência é " + cliente.getAgencia() +
+                ", conta " + cliente.getNumero() + " e seu saldo R$ " + cliente.getSaldo() + " já está disponível para saque.");
+
+        scan.close();
     }
 }
